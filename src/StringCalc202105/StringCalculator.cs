64e046a8d96ccd67f4b67522716a4fd1585ef50a
Steve Smith <steve@kentsmiths.com>
2021-05-19 23:27:29 -0400

    test 2 + refactor to theory

--- src/StringCalc202105/StringCalculator.cs
+++ src/StringCalc202105/StringCalculator.cs
@@ -6,7 +6,9 @@ namespace StringCalc202105
     {
         internal object Add(string numbers)
         {
-            return 0;
+            if (String.IsNullOrEmpty(numbers)) return 0;
+
+            return 1;
         }
     }
 }
--- src/StringCalc202105/StringCalculator_Add.cs
+++ src/StringCalc202105/StringCalculator_Add.cs
@@ -14,5 +14,16 @@ namespace StringCalc202105
 
             Assert.Equal(0, result);
         }
+
+        [Theory]
+        [InlineData("1", 1)]
+        public void Returns1GivenStringWith1(string numbers, int expectedResult)
+        {
+            var calculator = new StringCalculator();
+
+            var result = calculator.Add(numbers);
+
+            Assert.Equal(expectedResult, result);
+        }
     }
 }
